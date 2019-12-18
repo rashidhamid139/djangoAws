2554bdcf4e51be03c55288b422f5ae2de676c2f2
Rashid <rashidhamid139@gmail.com>
2019-12-18 23:40:01 +0530

    Third commit

--- StackAPI/urls.py
+++ StackAPI/urls.py
@@ -1,10 +1,8 @@
 from . import views
 from django.urls import path
 urlpatterns =[
-    path('', views.home, name='home'),
-    path('testcookie/', views.cookie_session, name='setcookie'),
-    path('deletecookie/', views.cookie_delete, name='deletecookie'),
+    # path('', views.home, name='home'),
     path('create/', views.create_session, name='createsession'),
-    path('access/', views.access_session1, name='accesssession'),
-    # path('post_comment', views.post_comment, name='comment'),
+    path('', views.access_session1, name='accesssession'),
+    path('delete', views.delete_session, name='delete'),
 ]
--- StackAPI/views.py
+++ StackAPI/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render, redirect
 import requests, json
 from django.core.paginator import Paginator
 from django.http import HttpResponse
-
 from math import ceil
 
 
@@ -33,7 +32,6 @@ def home(request ):
         request.session['time']=time()
     return render(request, 'StackAPI/home.html', {'contacts':contacts} )
 
-
 def access_session1(request):
     url = "https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow"
     resp = requests.get(url)
@@ -43,34 +41,29 @@ def access_session1(request):
     print("reqiest", request.session)
     contacts = paginator.get_page(page)
 
+    try:
+        if request.session['count']:
+            print("Inside Loop", request.session)
+            if ceil(request.session['time'] - time()) <= 60:
+                if request.session['count'] < 5:
+                    return render(request, 'StackAPI/home.html', {'contacts': contacts})
+                else:
+                    return render(request, 'StackAPI/warn.html')
+            else:
+                return render(request, 'StackAPI/warn.html')
 
+    except:
+        request.session['count']= 0
+        request.session['time']=time()
+    print(request.session['count'])
     if request.session.get('name') and request.session.get('password'):
+        request.session['count'] += 1
+
 
         return render(request, 'StackAPI/home.html', {'contacts': contacts})
         # return  HttpResponse(response)
     else:
         return redirect('createsession')
-
-
-
-
-
-def cookie_session(request):
-    request.session.set_test_cookie()
-    return HttpResponse("<h1>Testing Cookie</h1>")
-
-def cookie_delete(request):
-    if request.session.test_cookie_worked():
-        request.session.delete_test_cookie()
-        response = HttpResponse("Test <br> Cookie Created")
-    else:
-        response = HttpResponse("Testing: <br> Your Browser doesnt accept cookies")
-    return response
-
-
-
-
-
 
 
 
@@ -80,20 +73,19 @@ def create_session(request):
     return HttpResponse("<h1>Testing:<br> Session is set</h1>")
 
 
-
-# def access_session(request):
-#     response = "<h1>Welcome to Testing Session</h1><br>"
-#     if request.session.get('name'):
-#         response += "Name: {0}<br>".format(request.session.get('name'))
-#     if request.session.get('password'):
-#         response += "Password : {0}<br>".format(request.session.get('password'))
-#         return  HttpResponse(response)
-#     else:
-#         return redirect('createsession')
-#
+def delete_session(request):
+    try:
+        del request.session['name']
+        del request.session['password']
+    except KeyError:
+        pass
+    return HttpResponse("<center><h1><Strong>Session is Deleted</strong></h1></center>")
 
 
 
-
-
-
+def check_session(request):
+    if request.session.get('name'):
+        print(request.session['name'])
+        return redirect('createsession')
+    else:
+        return redirect('createsession')
